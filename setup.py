bc07fbaf4ac3e37459a96dfa69f0d55e338fe30d
Kelvin P <76895368+K-Tech-nology@users.noreply.github.com>
2022-09-01 15:09:14 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name = "preprocessing",
+    version = '1.0',
+    packages = setuptools.find_packages(),
+    url = 'https://github.com/K-Tech-nology/prelib',
+    license = '',
+    author = 'Kelvin Prawtama',
+    author_email = 'kelvinprawtama888@gmail.com',
+    description = 'Data Preprocessing'
+)
